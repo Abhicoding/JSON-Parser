257f8e99e54a905e035fbd241872a4c34861e004
Abhicoding <shinganeabhishek@gmail.com>
2018-01-11 11:35:00 +0530

    Added Array Parser that needs validity of the inside

--- json_parser.js
+++ json_parser.js
@@ -1,94 +1,110 @@
 /* wrting a json parser to verify it the given
-input is in valid json format*/ 
+input is in valid json format */
 
-//Let's change something to test.
+// Let's change something to test.
 
-//var jsonParse = (function(){
+// var jsonParse = (function(){
 
-"use strict"
- 
- //function main() {
-  //var r_temp = readLine();
-//}
+'use strict'
 
-	
+ // function main() {
+  // var r_temp = readLine();
+// }
+
 //  Test for null
-function parseNull(input){
-	let x = input.split(''), parseOut;
-	if (x[0] !== 'n'){
-		return null;
-	}else if(x.length < 4){
-      return null;
-    }else{
-        parseOut = [x.slice(0, 4).join('')].concat(x.slice(4).join(''));
-        //console.log(parseOut);
-		if (parseOut[0] == 'null'){			
-          return parseOut;
-		}else{
-			return null;
-		}
-	}
+function parseNull (input) {
+  let x = input.split(''), parseOut
+  if (x[0] !== 'n') {
+    return null
+  } else if (x.length < 4) {
+    return null
+  } else {
+    parseOut = [x.slice(0, 4).join('')].concat(x.slice(4).join(''))
+    // console.log(parseOut);
+    if (parseOut[0] == 'null') {
+      return parseOut
+    } else {
+      return null
+    }
+  }
 }
-
-
 
 // 	Test for Boolean
-function parseBoolean(input){
-	let x = input.split(''), parseOut;
-	if (x[0] == 't'){		
-		if (x.length < 4){
-			return null;
-		}else{
-			parseOut = [x.slice(0, 4).join('')].concat(x.slice(5).join(''));
-			if (parseOut[0] == 'true'){
-				return parseOut;
-			}
-		}return null;
-	}else if (x[0] == 'f'){
-		if (x.length < 5){
-			return null;
-		}else{
-			parseOut = [x.slice(0, 5).join('')].concat(x.slice(6).join(''));
-			if (parseOut[0] == 'false'){
-				return parseOut;
-			}
-		}return null;
-	}
+function parseBoolean (input) {
+  let x = input.split(''), parseOut
+  if (x[0] == 't') {
+    if (x.length < 4) {
+      return null
+    } else {
+      parseOut = [x.slice(0, 4).join('')].concat(x.slice(5).join(''))
+      if (parseOut[0] == 'true') {
+        return parseOut
+      }
+    } return null
+  } else if (x[0] == 'f') {
+    if (x.length < 5) {
+      return null
+    } else {
+      parseOut = [x.slice(0, 5).join('')].concat(x.slice(6).join(''))
+      if (parseOut[0] == 'false') {
+        return parseOut
+      }
+    } return null
+  }
 }
 
-
-
 //	Test for String
-function parseString(input){
-  let reg = /\"([^"]*)\"/;
-  let parseOut = input.match(reg);
-  console.log(parseOut);
-  if(parseOut == null || Number(parseOut.index) !== 0) {
-  	return null;
-  }else{
-  return [parseOut[1], input.slice(parseOut[1].length+2)];	
-  }  
+function parseString (input) {
+  let reg = /\"([^"]*)\"/
+  let parseOut = input.match(reg)
+  console.log(parseOut)
+  if (parseOut == null || Number(parseOut.index) !== 0) {
+  	return null
+  } else {
+    return [parseOut[1], input.slice(parseOut[1].length + 2)]
+  }
 }
 
 // 	Test for Number
-function parseNumber(input){
-  let reg = /-?(\d+.)?\d+([eE][+-]?\d+)?/
-  let parseOut = input.match(reg);
-  console.log(parseOut);
-  if(parseOut == null || parseOut[0] == undefined) {
-  	return null;
-  }else{
-    return [parseOut[0], input.slice(parseOut[0].length)];	
-  }  
+function parseNumber (input) {
+  let reg = /\"?^(\-?\d+(\.\d+)?([eE][+-]?\d+)?)/
+  let parseOut = input.match(reg)
+  // console.log(parseOut);
+  if (parseOut == null || parseOut[0] == undefined) {
+  	return null
+  } else {
+    return [parseOut[0], input.slice(parseOut[0].length)]
+  }
 }
 
-// 	Test for Object (JSON object)
 // 	Test for Array
+function parseArray (input) {
+  let reg = /\[(.*)\]/
+  let parseOut = input.match(reg)
+  // console.log(parseOut);
+  if (parseOut == null || parseOut[0] == undefined) {
+  	return null
+  } else {
+    return [parseOut[0], input.slice(parseOut[0].length)]
+  }
+}
 
+function parseSpace (input) {
+  let reg = /(^\s*)/
+  let parseOut = input.match(reg)
+  if (parseOut == null || parseOut[0] == undefined) {
+  	return null
+  } else if (parseOut[0] == '') {
+  	return null
+  } else {
+    return [parseOut[0], input.slice(parseOut[0].length)]
+  }
+}
+// 	Test for Object (JSON object)
 //	Test for function
 //	Test for date
 //	Test for undefined
-//console.log(parseString("\"efg\n\"abcd"));
-console.log(parseNumber("-1.-E-1"));
-
-//}
+// console.log(parseString("\"efg\n\"abcd"));
+console.log(parseArray('[]"-1.-E-1"'))
+console.log(parseBoolean('trueeee'))
+// }
