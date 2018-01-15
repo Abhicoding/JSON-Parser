5e24c5ad58f0128e477df6df753dd136275ec6ae
Abhicoding <shinganeabhishek@gmail.com>
2018-01-15 09:01:15 +0530

    Removed some unnecessary lines

--- json_parser.js
+++ json_parser.js
@@ -1,5 +1,4 @@
-/* wrting a json parser to verify it the given
-input is in valid json format */
+/* wrting a json parser to verify it the given input is in valid json format */
 
 // Let's change something to test.
 
@@ -7,11 +6,6 @@ input is in valid json format */
 
 'use strict'
 
- // function main() {
-  // var r_temp = readLine();
-// }
-
-//  Test for null
 var value = function (input) {
   if (input[0] == ' ') {
     return null
@@ -37,8 +31,7 @@ function parseNull (input) {
   } else if (x.length < 4) {
     return null
   } else {
-    parseOut = [x.slice(0, 4).join('')].concat(x.slice(4).join(''))
-    // console.log(parseOut);
+    parseOut = [x.slice(0, 4).join('')].concat(x.slice(4).join(''))// console.log(parseOut);
     if (parseOut[0] == 'null') {
       return parseOut[1]
     } else {
@@ -47,7 +40,6 @@ function parseNull (input) {
   }
 }
 
-//  Test for Boolean
 function parseBoolean (input) {
   let x = input, parseOut
   if (x[0] == 't') {
@@ -71,11 +63,9 @@ function parseBoolean (input) {
   }
 }
 
-//  Test for String
 function parseString (input) {
   let reg = /\"([^"]*)\"/
-  let parseOut = input.match(reg)
-  // console.log(parseOut)
+  let parseOut = input.match(reg)// console.log(parseOut)
   if (parseOut == null || Number(parseOut.index) !== 0) {
     return null
   } else {
@@ -83,11 +73,9 @@ function parseString (input) {
   }
 }
 
-//  Test for Number
 function parseNumber (input) {
   let reg = /\"?^(\-?\d+(\.\d+)?([eE][+-]?\d+)?)/
-  let parseOut = input.match(reg)
-  // console.log(parseOut[0])
+  let parseOut = input.match(reg)// console.log(parseOut[0])
   if (parseOut == null || parseOut[0] == undefined) {
     return null
   } else {
@@ -95,17 +83,13 @@ function parseNumber (input) {
   }
 }
 
-//  Test for Array
 function parseArray (input) {
   let reg = /\[(.*)\]/
   let parseOut = input.match(reg)
   if (parseOut == null || parseOut[0] == undefined) {
     return null
   } else {
-    // console.log()
-    /* if (parseOut[1][0] = '[') {
-      return parseArray(parseOut[1])
-    } else */if (arrayHelper(parseOut[1])) {
+    if (arrayHelper(parseOut[1])) {
       return input.slice(parseOut[0].length)
     } else {
       return null
@@ -114,7 +98,7 @@ function parseArray (input) {
 }
 
 function parseSpace (input) {
-  let reg = /(^\s*)/
+  let reg = /(^\s+)/
   let parseOut = input.match(reg)
   if (parseOut == null || parseOut[0] == undefined) {
     return null
@@ -125,14 +109,9 @@ function parseSpace (input) {
   }
 }
 
-// console.log(parseString("\"efg\n\"abcd"));
-// let x = parseArray('[]-1.-E-1"')
-// console.log(parseNumber(parseArray(x[0])[0]))
-
 function parseComma (input) {
   let reg = /^\,/
-  let parseOut = input.match(reg)
-  // console.log(parseOut)
+  let parseOut = input.match(reg)// console.log(parseOut)
   if (parseOut == null || parseOut[0] == undefined) {
     return null
   } else if (parseOut[0] == '') {
@@ -146,18 +125,14 @@ function arrayHelper (input) {    // Takes insides of an array excluding []
   let x = input, commacount, objcount = 0, i = 0
 
   while (i == 0) {
-    // console.log(x, x == '')
-    // console.log(typeof (x), parseComma(x), parseSpace(x), objcount, commacount)
     if (x == '' && (commacount == 0 || commacount == undefined)) {
       return true
     } else if (value(x) != null && x != '') {
       x = value(x)
-      // console.log('found a value')
       objcount += 1
       commacount = 0
     } else if (parseSpace(x) != null && (objcount <= 1)) {
-      x = parseSpace(x)
-      // console.log('was in space')
+      x = parseSpace(x)// console.log('was in space')
     } else if (parseComma(x) && objcount == 1 && commacount == 0) {
       x = parseComma(x)
       commacount = 1
@@ -168,22 +143,8 @@ function arrayHelper (input) {    // Takes insides of an array excluding []
   }
 }
 
-// if (parseArray(x[0][0])) {
-  // console.log(parseArray(x[0][0]))
-// console.log(parseNumber(parseArray(x[0][0])[0]))
-// }
-/* if (parseNumber(x[0])) {
-  x = parseArray(x[1])
-  console.log(x)
-} */
-// console.log(parseSpace(' ') == '')
-console.log(value(''))
-console.log(value('nul'))
-console.log(value('nulL'))
-console.log(value('anull'))
-// console.log(arrayHelper('1'))
-// console.log(value('1') != null)
-
-// console.log('\"abc\"', 'strig experiment')
-// console.log(arrayHelper(['"abcd", 1234', 'just some things']))
-// }
+console.log(value('[[1, 2, 3], 12abc'))
+console.log(value('truea'))
+console.log(value('false'))
+console.log(value('null'))
+console.log(parseSpace('null'))
