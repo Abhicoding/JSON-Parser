36a35078b9a8c48a24d509e05de1b9da4c7da3b8
Abhicoding <shinganeabhishek@gmail.com>
2018-09-02 01:51:55 +0530

    Reduced number of lines attempt 1

--- JSON-Reader.js
+++ JSON-Reader.js
@@ -1,8 +1,17 @@
 var parser = require('./json_parser.js') 
 
 var fs = require("fs");
+fs.readFile("./Test/JSONfile1.js", 'utf-8', function (err, data) {
+    if (err) throw err;
+    if (parser.parse(data) === null) return `Couldn't parse`
+});
 
 fs.readFile("./Test/JSONfile2.js", 'utf-8', function (err, data) {
     if (err) throw err;
-    console.log(parser.parse(data));
+    if (parser.parse(data) === null) return `Couldn't parse`
+});
+
+fs.readFile("./Test/test.js", 'utf-8', function (err, data) {
+    if (err) throw err;
+    if (parser.parse(data) === null) return `Couldn't parse`
 });
--- json_parser.js
+++ json_parser.js
@@ -1,42 +1,31 @@
 /* wrting a json parser to verify it the given input is in valid json format */
 
-const valueParser = factoryParser([parseNull, parseBoolean, parseNumber, parseString, parseArray, parseObject])
+const valueParser = factoryParser(parseNull, parseBoolean, parseNumber, parseString, parseArray, parseObject)
 
-function factoryParser (args) {
+function factoryParser (...args) {
   return function (input) {
     for (let x of args) {
-      if (x(input)) {
-        return x
-      }
+      if (x(input)) return x
     }
-     return null
+    return null
   }
 }
 
 function parseNull (input) {
-  if (input.substr(0, 4) === 'null') {
-    return [null, input.substr(4)]
-  }
-  return null
+  return input.startsWith('null') ? [null, input.substr(4)] : null
 }
 
 function parseBoolean (input) {
-  if (input.substr(0, 4) === 'true') {
-    return [true, input.substr(4)]
-  } else if (input.substr(0, 5) === 'false') {
-    return [false, input.substr(5)]
-  }
-  return null
+  if (input.substr(0, 4) === 'true') return [true, input.substr(4)]
+  return input.startsWith('false') ? [false, input.substr(5)] : null
 }
 
 function parseString (input) {
-  if (input[0] == '\"') {
+  if (input[0] === '\"') {
     input = input.substr(1)
     let parseOut = ''
     while (input[0] !== '\"') {
-      if (input == '') {
-        return null
-      }
+      if (input == '') return null
       if (input[0] == '\\'){
         parseOut += input.substr(0, 2)
         input = input.substr(2)
@@ -51,39 +40,24 @@ function parseString (input) {
 
 function parseNumber (input) {
   let reg = /^(\-?\d+(\.\d+)?([eE][+-]?\d+)?)/, parseOut = input.match(reg)
-  if (parseOut) {
-    return [Number(parseOut[0]), input.slice(parseOut[0].length)]
-  }
-  return null
+  return parseOut ? [Number(parseOut[0]), input.slice(parseOut[0].length)] : null
 }
 
 function parseArray (input) {
   if (input[0] == '[') {
-    let result = input, parseOut = []
-    result = result.substr(1)
+    let result = input.substr(1), parseOut = [], temp
     while (result[0] !== ']') {
-      if (parseSpace(result) !== null) {
-        result = parseSpace(result)[1]
-      } else {
-        if (valueParser(result) !== null){
-          result = valueParser(result)(result)
-          parseOut.push(result[0])
-          result = result[1]
-        }else{
-          return null
-        }
-        if (parseSpace(result) !== null) {
-          result = parseSpace(result)[1]
-        }
-        if (result[0] == ',') {
-          result = parseComma(result)[1]
-          if (parseSpace(result) !== null) {
-            result = parseSpace(result)[1]
-          }
-          if (valueParser(result) !== null) {
-            continue
-          } else { return null }
-        }
+      if (parseSpace(result) !== null) result = parseSpace(result)[1]
+      if (valueParser(result) !== null) {
+        [temp, result] = valueParser(result)(result)
+        parseOut.push(temp)
+      }
+      if (parseSpace(result) !== null) result = parseSpace(result)[1]
+      if (result[0] == ',') {
+        result = result.substr(1)
+        if (parseSpace(result) !== null) result = parseSpace(result)[1]
+        if (valueParser(result) !== null) continue
+        else return null
       }
     } return [parseOut].concat(result.substr(1))
   } return null
@@ -91,90 +65,42 @@ function parseArray (input) {
 
 function parseSpace (input) {
   let reg = /(^[\s\t\n\r]+)/, parseOut = input.match(reg)
-  if (parseOut) {
-    return [parseOut[0], input.slice(parseOut[0].length)]
-  }
-  return null
-}
-
-function parseComma (input) {  // don't really need this
-  let reg = /^\,/, parseOut = input.match(reg)
-  if (parseOut) {
-    return [parseOut[0], input.slice(parseOut[0].length)]
-  }
-  return null
-}
-
-function parseColon (input) { // don't really need this
-  let reg = /^\:/, parseOut = input.match(reg)
-  if (parseOut) {
-    return [parseOut[0], input.slice(parseOut[0].length)]
-  }
-  return null
+  return parseOut ? [parseOut[0], input.slice(parseOut[0].length)] : null
 }
 
 function parseObject (input) {
   if (input[0] == '{') {
-    let result = input, r1, r2, parseOut = {}
-    result = result.substr(1)
+    let result = input.substr(1), r1, r2, parseOut = {}
     while (result[0] !== '}') {
-      if (parseSpace(result) !== null) {
-        result = parseSpace(result)[1]
-      } else {
-        if (parseString(result) !== null){
-          result = parseString(result), r1 = result[0]
-          result = result[1], commacount = 0
-          if (parseSpace(result) !== null) {
-            result = parseSpace(result)[1]
-          }
-          if (result[0] == ':') {
-            result = parseColon(result)[1]
-            if (parseSpace(result) !== null) {
-              result = parseSpace(result)[1]
-            }
-            if(valueParser(result) !== null){
-              result = valueParser(result)(result), r2 = result[0]
-              result = result[1], parseOut[r1] = r2
-            }else{
-              return null
-            }
-            if (parseSpace(result) !== null) {
-              result = parseSpace(result)[1]
-            }
-            if (result[0] == ',') {
-              result = parseComma(result)[1]
-              if (parseSpace(result) !== null) {
-                result = parseSpace(result)[1]
-              }
-              if (result[0] == ']') {
-                return null
-              }
-              if (result[0] !== '"') { return null }
-            } else { continue }
-          } else { return null }
-        } else { return null }
-      }
+      if (parseSpace(result) !== null) result = parseSpace(result)[1]
+      if (parseString(result) !== null) {
+        [r1, result] = parseString(result)
+        if (parseSpace(result) !== null) result = parseSpace(result)[1]
+        if (result[0] == ':') {
+          result = result.substr(1)
+          if (parseSpace(result) !== null) result = parseSpace(result)[1]
+          if(valueParser(result) !== null) {
+            [r2, result] = valueParser(result)(result), parseOut[r1] = r2
+          } else return null
+          if (parseSpace(result) !== null) result = parseSpace(result)[1]
+          if (result[0] == ',') result = result.substr(1)
+          else continue
+        } else return null
+      } else return null
     } return [parseOut].concat(result.substr(1))
   } return null
 }
 
 const jsonParse = function (input) {
-  let x
-  if(parseSpace(input) !== null){
-    input = parseSpace(input)[1]
-  }
+  if(parseSpace(input) !== null) input = parseSpace(input)[1]
   if (valueParser(input) !== null) {
-    x = valueParser(input)(input)
+    var x = valueParser(input)(input)
     if(parseSpace(x[1]) !== null){
-      let y = parseSpace(x[1])
+      var y = parseSpace(x[1])
       x = [x[0], y[1]]
     }
-    if (x[1] === '') {
-      return x[0]
-    } else {
-      console.log("Error: couldn't parse completely due to invalid JSON :")
-      return x
-    }
+    if (x[1] === '') return x[0]
+    return x
   } return 'Error: Invalid JSON '
 }
 
@@ -185,10 +111,29 @@ exports.parse = jsonParse
 //console.log(parseObject('{"favorited" : false}'))
 //console.log(parseString('"abc\\\"c"d"'))
 //console.log(parseObject('{ "a" : }'))
-/*
-console.log(parseArray('[ "a" ,, "b"]'))
-console.log(parseArray('[ "a" 1 , "b"]'))
-console.log(parseArray('[ "a" , "b"]]'))
-console.log(parseArray('[ "a" ,]'))
-console.log(parseArray('[ "a" , ["b"]]'))
-console.log(parseArray('[["created_at", "Thu Jun 22 21:00:00 +0000 2017"]]'))*/
+// console.log(parseNumber('[ "a" ,, "b"]')) //null
+// console.log(parseNumber('[ "a" 1 , "b"]'))
+// console.log(parseArray('[ "a" , "b"]]'))
+// console.log(parseArray('[ "a" ,]')) //null
+// console.log(parseArray('[ "a" , ["b"]]'))
+// console.log(parseArray('[[\"created_at\", \"Thu Jun 22 21:00:00 +0000 2017\"]]', 'parsearray'))
+
+// function parseArray2 (input) {
+//   if (input[0] !== '[') return null
+//   var result = input.substr(1), parseOut = [], temp = ''
+//   while (result[0] !== ']') {
+//     if (parseSpace(result)) result = parseSpace(input)[1]
+//     console.log(result, 'array2')
+//     if (valueParser(result)){
+//       [temp, result] = valueParser(result)(result)
+//       parseOut.push(temp)
+//       if (parseSpace(result)) result = parseSpace(input)[1]
+//       if (result[0] === ',') {
+//         result = input.substr(1)
+//         continue
+//       }
+//       if (result[0] !== ']') return null
+//       continue
+//     } return null
+//   } return [parseOut, result.substr(1)]
+// }
